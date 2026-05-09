775e72d4aa66885240547e2c530319ea6632e09a
Boyu-Feng <fengboyu1009@gmail.com>
2026-05-09 16:28:37 +0800

    59

--- run_browsecomp_bm25_experiments.sh
+++ run_browsecomp_bm25_experiments.sh
@@ -27,13 +27,13 @@ mkdir -p "$LOGDIR"
 # $PYTHON $SCRIPT --experiment rag --data_path "$DATA_PATH" --retriever_backend browsecomp_bm25 --browsecomp_index_dir "$INDEX_DIR" > "${LOGDIR}/logs_rag_browsecomp_bm25.log" 2>&1
 # echo "$(date): BrowseComp RAG done."
 
-# echo "$(date): Starting BrowseComp ReAct experiment..."
-# $PYTHON $SCRIPT --experiment react --data_path "$DATA_PATH" --retriever_backend browsecomp_bm25 --browsecomp_index_dir "$INDEX_DIR" > "${LOGDIR}/logs_react_browsecomp_bm25.log" 2>&1
-# echo "$(date): BrowseComp ReAct done."
+echo "$(date): Starting BrowseComp ReAct experiment..."
+$PYTHON $SCRIPT --experiment react --data_path "$DATA_PATH" --retriever_backend browsecomp_bm25 --browsecomp_index_dir "$INDEX_DIR" > "${LOGDIR}/logs_react_browsecomp_bm25.log" 2>&1
+echo "$(date): BrowseComp ReAct done."
 
-echo "$(date): Starting BrowseComp ReAct-KV (Step-Aware H2O, BrowseComp BM25) experiment..."
-$PYTHON $SCRIPT --experiment react_kv_step_aware_h2o --max_steps 40 --data_path "$DATA_PATH" --retriever_backend browsecomp_bm25 --browsecomp_index_dir "$INDEX_DIR" > "${LOGDIR}/logs_react_kv_step_aware_h2o_browsecomp_bm25.log" 2>&1
-echo "$(date): BrowseComp ReAct-KV (Step-Aware H2O, BrowseComp BM25) done."
+# echo "$(date): Starting BrowseComp ReAct-KV (Step-Aware H2O, BrowseComp BM25) experiment..."
+# $PYTHON $SCRIPT --experiment react_kv_step_aware_h2o --max_steps 40 --data_path "$DATA_PATH" --retriever_backend browsecomp_bm25 --browsecomp_index_dir "$INDEX_DIR" > "${LOGDIR}/logs_react_kv_step_aware_h2o_browsecomp_bm25.log" 2>&1
+# echo "$(date): BrowseComp ReAct-KV (Step-Aware H2O, BrowseComp BM25) done."
 
 # echo "$(date): Starting BrowseComp ReAct-KV (Step-Inter, BrowseComp BM25) experiment..."
 # $PYTHON $SCRIPT --experiment react_kv_step_inter --data_path "$DATA_PATH" --retriever_backend browsecomp_bm25 --browsecomp_index_dir "$INDEX_DIR" > "${LOGDIR}/logs_react_kv_step_inter_browsecomp_bm25.log" 2>&1
